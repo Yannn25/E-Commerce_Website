9939e9291305fe1ff6aacf4712f3820babe3fc9b
Marie-Tiffany Emmou Coffi <marie-tiffanyemmoucoffi@MacBook-Pro-de-Marie-Tiffany.local>
2023-04-17 20:47:04 +0200

    début

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
-const container = document.getElementById('container');
-const signUpButton = document.getElementById('signUp');
-const logInButton = document.getElementById('logIn');
-
-signUpButton.addEventListener('click', ()=>{
-    container.classList.add('panel-active')
-})
-
-logInButton.addEventListener('click', ()=>{
-    container.classList.remove('panel-active')
-})
+function updateFooterPosition() {
+    const footer = document.querySelector('.footer');
+    if (window.innerHeight > footer.offsetTop + footer.offsetHeight) {
+      footer.style.position = 'fixed';
+      footer.style.bottom = '0';
+    } else {
+      footer.style.position = 'static';
+    }
+  }
+  
+  // Mettre à jour la position du footer au chargement de la page
+  window.addEventListener('load', updateFooterPosition);
+  
+  // Mettre à jour la position du footer lorsque la taille de la fenêtre change
+  window.addEventListener('resize', updateFooterPosition);
