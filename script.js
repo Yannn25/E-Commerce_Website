206c54c4a0fe2eec4ad85d1386879d82040feed0
Marie-Tiffany Emmou Coffi <marie-tiffanyemmoucoffi@MacBook-Pro-de-Marie-Tiffany.local>
2023-04-17 21:21:05 +0200

    début

--- script.js
+++ script.js
@@ -4,16 +4,18 @@ var ContactButton = document.getElementById("SwitchtoContact");
 
 var section1 = document.getElementById("accueil");
 var section2 = document.getElementById("connect");
-
+var section3 = document.getElementById("jsp");
 ConnectButton.addEventListener("click", function() {
   if (section2.style.display === "none") {
-    section2.style.display = "flex";
+    section2.style.display = "block";
     section1.style.display = "none";
+    section3.style.display = "none";
   }
 });
 HomeButton.addEventListener("click", function() {
   if (section1.style.display === "none") {
     section1.style.display = "flex";
+    section3.style.display = "flex";
     section2.style.display = "none";
   }
 });
