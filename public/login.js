0900a11a1546f477f484b3073fc7437bec1e506d
Marie-Tiffany Emmou Coffi <marie-tiffanyemmoucoffi@eduroam-prg-gm-1-2-239.net.univ-paris-diderot.fr>
2023-05-23 19:27:57 +0200

    pages

--- public/login.js
+++ public/login.js
@@ -1,3 +1,28 @@
+/**
+ * transition 3 barres croix pour la navbar
+ */
+const tg = document.querySelector(".hmg")
+const navlinksContainer = document.querySelector(".navlinks-container")
+const tgNav = e =>{
+	tg.classList.toggle("open")
+	navlinksContainer.classList.toggle("open")
+	const ariaTg = tg.getAttribute("aria-expanded") === "true"?"false" : "true"
+	tg.setAttribute("aria-expanded",ariaTg)
+}
+tg.addEventListener("click",tgNav)
+
+/**
+ * pour que ce soit plus propre quand on resize
+ */
+
+new ResizeObserver(entries => {
+	if (entries[0].contentRect.width <= 900) {
+		navlinksContainer.style.transition = "transform 0.3s ease-out"
+	}else{
+		navlinksContainer.style.transition = "none"
+	}
+	
+}).observe(document.body)
 const signUp =  document.querySelector('.t-signup');
 const logIn = document.querySelector('.t-login');
 const addclass = document.querySelector('.site')
